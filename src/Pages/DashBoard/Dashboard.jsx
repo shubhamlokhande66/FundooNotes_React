2b3df9d04842d059331ef08d1a986fa4f5b15a1a
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-01-13 07:56:27 +0530

    [Shubham]Refactor :Add note and Display note Functionality and import file

--- src/Pages/DashBoard/Dashboard.jsx
+++ src/Pages/DashBoard/Dashboard.jsx
@@ -1,142 +1,170 @@
 
 
-import {
-  Card,
-  CardContent,
-  CircularProgress,
-  Container,
-  Grid,
-  Typography,
-} from "@material-ui/core";
-import React, { useEffect, useState } from "react";
-import ImageIcon from "@material-ui/icons/ImageOutlined";
-import CheckBoxOutlinedIcon from "@material-ui/icons/CheckBoxOutlined";
-import "./Dashboard.css";
-import CreateNote from "../../components/CreateNotes/CreateNotes";
-import { getNoteList } from "../../Service/NoteService";
-import DisplayCard from "../../components/DisplayNotes/DisplayNotes";
+import React, { useState, useEffect } from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import './Dashboard.css';
+import AddNote from '../../components/CreateNotes/CreateNotes'
+import DisplayNotes from '../../components/DisplayNotes/DisplayNotes';
+import NoteService from '../../Service/NoteService';
 import Appbar from "../../components/AppBar/Appbar";
-import Collaborator from "../../components/Collaborator/Collaborator";
 
-  const DashBoard = () => {
-    const [showCard, setShowCard] = useState("take_note");
-    const [noteList, setNoteList] = useState([]);
-    const [collabUser, setCollabUser] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [selectCard, setSelectCard] = useState('note')
-    const [pinText, setPinText] = useState(false)
-    const [refresh, setRefresh] = useState(Math.random())
-    const [searchNote, setSearchNote] = useState('')
-    const [isGrid, setIsGrid] = useState(true)
-    const [showCheckBox, setShowCheckBox] = useState(true)
-   
-    useEffect(() => {
-      getNoteList()
-        .then((res) => {
-          setNoteList(res.data.data.data);
-          setIsLoading(false);
+const service = new NoteService();
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  search: {
+    alignItems: 'center',
+    position: 'relative',
+    borderRadius: '5px',
+    // borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.black, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.black, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '40%',
+    height: '45px',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: '55%',
+      height: '45px',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    
+  },
+  inputInput: {
+    width: '70%',
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('md')]: {
+      width: '40ch',
+    },
+  },
+  paper: {
+    marginRight: theme.spacing(1),
+
+  },
+  hide: {
+    display: 'menuIcon',
+  },
+  drawer: {
+    // zIndex: '999',
+    width: drawerWidth,
+    paddingLeft: '8px',
+    paddingTop: '60px',
+    // flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxShadow: 'none',
+    borderBottom: '1px',
+    flex: 'none'
+  },
+  drawerOpen: {
+    zIndex: '999',
+    paddingTop: '60px',
+    paddingLeft: '8px',
+    border: 'none',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    zIndex: '999',
+    paddingTop: '60px',
+    paddingLeft: '8px',
+    border: 'none',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+      paddingLeft: '3px',
+    },
+    '@media(maxWidth: 600px)' : {
+      paddingLeft: '3px',
+    }
+  },
+  appbar:{
+    boxShadow: 'none',
+    borderBottom: '1px solid lightgray' 
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    marginTop: '3%',
+    flexGrow: 1,
+    // paddingRight: 200, 
+  },
+
+}));
+
+export default function Dashboard() {
+
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+  const [open, setOpen] = React.useState(false);
+  const [notes,setNotes]= React.useState([]);
+  const [noteList, setNoteList] = useState([]);
+
+ 
+
+  const getNote = () => {
+    service.getNotes().then((res) => {
+            setNoteList(res.data.data.data.filter(item => item.isDeleted === false));
         })
         .catch((err) => {
-          console.warn("error", err);
+            console.log(err);
         });
-    }, [showCard,refresh]);
-  
-    
-  
-    return (
-      <div>
-        <Appbar selectCard={selectCard} 
-        setSelectCard={setSelectCard} setRefresh={setRefresh} 
-        setSearchNote={setSearchNote} setIsGrid={setIsGrid} isGrid={isGrid}/>
-        <div className="noteContainer">
-          {(showCard === "take_note" ) ? (
-            <Card
-              className="cardContainer"
-              onClick={() => setShowCard("create_note")}
-            >
-              <CardContent className="subCardContainer">
-                <Typography className="noteTitle">Take a note...</Typography>
-                <div className="imageCheckBoxContainer">
-                  <CheckBoxOutlinedIcon onClick={() => setShowCheckBox(false)}
-                   style={{ cursor: 'pointer' }}/>
-                  <ImageIcon />
-                </div>
-              </CardContent>
-            </Card>
-          ) : null}
-          {showCard === "create_note" ? (
-            <CreateNote collabUser={collabUser} setShowCard={setShowCard} 
-            setRefresh={setRefresh} setShowCheckBox={setShowCheckBox} showCheckBox={showCheckBox}/>
-          ) : null}
-          {showCard === "collaborator" ? (
-            <Collaborator
-              setCollabUser={setCollabUser}
-              setShowCard={setShowCard}
-            />
-          ) : null}
-        </div>
-        {isLoading ? (
-          <div className="progressBar">
-            <CircularProgress color="primary" />
-          </div>
-        ) : (
-          <div>
-            <Container className="displayCardContainer">
-            {pinText ? <div className="pinText">Pin</div>: null}
-             {searchNote === '' ?
-               <Grid
-                container
-                className="gridContainer"
-                spacing={2}
-                direction="row"
-                alignItems="center"
-              >
-                
-              </Grid> : null}
-              {pinText ? <div className="otherText">Other</div>: null}
-              {searchNote === '' ?
-                <Grid
-                container
-                spacing={2}
-                direction="row"
-                alignItems="center"
-                className="gridContainer"
-              >
-                {noteList.map((item, index) => {         
-                if(selectCard === 'note'){
-                  return(
-                    <React.Fragment key={index}>
-                      {!item.isPined && !item.isArchived && item.isDeleted === false ?
-                      <DisplayCard key={index} item={item} setRefresh={setRefresh} setPinText={setPinText} isGrid={isGrid}/>:null}
-                    </React.Fragment>
-                  )
-                }
-   
-                 return ( 
-                   <React.Fragment key={index}>
-                     {item.noteLabels.map((labelItem,labelIndex)=>{
-                       if(labelItem.label === selectCard){
-                        return(
-                          <DisplayCard key={labelIndex} item={item} setRefresh={setRefresh} setPinText={setPinText} isGrid={isGrid}/>
-                        )
-                      }
-                      return(
-                        <div >
-                          {false &&
-                        <DisplayCard key={index} item={item} />}
-                        </div>
-                      )
-                      })}
-                  </React.Fragment>
-                    )
-                  }
-                )}
-              </Grid> : null}
-              </Container>
-          </div>
-        )}
+}
+
+
+useEffect(() => {
+    getNote()
+}, []);
+
+
+
+  return (
+    <div className="main">
+      
+      <div className={classes.content}>
+        <AddNote GetNote={getNote} />
+        < Appbar/>
+        <DisplayNotes item={noteList} GetNote={getNote} />
       </div>
-    );
-  };
-  
-  export default DashBoard;
+      </div>
+  )
+
+}
