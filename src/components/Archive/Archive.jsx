371df6b8845d3a9348d699d13a9412ef185b0ca9
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-02-25 16:20:00 +0530

    [Shubham]Add.Removed Unwanted Code and Proper Formatting

--- src/components/Archive/Archive.jsx
+++ src/components/Archive/Archive.jsx
@@ -1,32 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import DisplayNote from '../DisplayNotes/DisplayNotes';
-import Service from '../../Service/NoteService';
-import './Archive.css'
-const services = new Service()
+import React, { useState, useEffect } from "react";
+import DisplayNote from "../DisplayNotes/DisplayNotes";
+import Service from "../../Service/NoteService";
+import "./Archive.css";
+const services = new Service();
 
 export default function Trash() {
-    const [archiveNote, setArchiveNote] = useState([]);
-    let archive = true;
+  const [archiveNote, setArchiveNote] = useState([]);
+  let archive = true;
 
-    const getArchiveNote = () => {
-        services.getArchiveNoteList()
-            .then((res) => {
-                setArchiveNote(res.data.data.data.reverse())
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }
+  const getArchiveNote = () => {
+    services
+      .getArchiveNoteList()
+      .then((res) => {
+        setArchiveNote(res.data.data.data.reverse());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    useEffect(() => {
-        getArchiveNote();
-    }, []);
+  useEffect(() => {
+    getArchiveNote();
+  }, []);
 
-    return (
-        <div className='Archive'>
-            <div className='archive'>
-            <DisplayNote item={archiveNote} GetNote={getArchiveNote} archive={archive} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="Archive">
+      <div className="archive">
+        <DisplayNote
+          item={archiveNote}
+          GetNote={getArchiveNote}
+          archive={archive}
+        />
+      </div>
+    </div>
+  );
 }
