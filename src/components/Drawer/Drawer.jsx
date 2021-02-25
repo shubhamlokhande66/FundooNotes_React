c41d8cbc9565737c952ffea7d49137e0fd8bc661
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-02-25 16:32:38 +0530

    [Shubham] Refactor.Removed uwanted code

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,21 +1,23 @@
-import React,{ useState, useEffect }from "react";
-import "./Drawer.css"
-import Drawer from '@material-ui/core/Drawer';
-import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
-import AddAlertIcon from '@material-ui/icons/AddAlert';
-import ArchiveIcon from '@material-ui/icons/Archive';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import { Divider, List, ListItem, Typography, ListItemIcon, ListItemText } from '@material-ui/core/';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import ArchiveOutlinedIcon from '@material-ui/icons/ArchiveOutlined';
-import EmojiObjectsOutlinedIcon from '@material-ui/icons/EmojiObjectsOutlined';
+import React, { useState, useEffect } from "react";
+import "./Drawer.css";
+import Drawer from "@material-ui/core/Drawer";
+import AddAlertIcon from "@material-ui/icons/AddAlert";
+import EditIcon from "@material-ui/icons/Edit";
+import {
+  Divider,
+  List,
+  ListItem,
+  Typography,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core/";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import ArchiveOutlinedIcon from "@material-ui/icons/ArchiveOutlined";
+import EmojiObjectsOutlinedIcon from "@material-ui/icons/EmojiObjectsOutlined";
 import { useHistory } from "react-router-dom";
-import { Link } from 'react-router-dom'
-import { Switch, Route } from 'react-router-dom'
-import Note from '../DisplayNotes/DisplayNotes'
-import Delete from '@material-ui/icons/Delete';
-import NoteService from '../../Service/NoteService';
+import { Link } from "react-router-dom";
+import Delete from "@material-ui/icons/Delete";
+import NoteService from "../../Service/NoteService";
 
 const service = new NoteService();
 const drawerWidth = 200;
@@ -26,38 +28,38 @@ const useStyles = makeStyles((theme) => ({
   },
   drawerOpen: {
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
   drawerClose: {
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    overflowX: 'hidden',
+    overflowX: "hidden",
     width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: theme.spacing(9) + 1,
     },
   },
   content: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
     marginLeft: 0,
   },
   root: {
-    display: 'flex',
+    display: "flex",
     height: 3,
   },
 
@@ -79,36 +81,25 @@ const useStyles = makeStyles((theme) => ({
   appBar: {
     width: 500,
   },
-}))
+}));
 export default function DrawerBar(props) {
   const classes = useStyles();
   const [keyValue, setKeyValue] = useState(false);
   const [noteList, setNoteList] = useState([]);
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
   let history = useHistory();
   const handleClass = (value) => {
     setKeyValue(value);
-  }
+  };
   useEffect(() => {
-    handleClass('Notes')
+    handleClass("Notes");
     history.push("/dashboard/notes");
-  }, [])
-
- 
-
-//   const getNote = () => {
-//     service.getNotes().then((res) => {
-//             setNoteList(res.data.data.data);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-// }
-
+  }, []);
 
   return (
     <div className={classes.root}>
-      <Drawer  open={props.drawerOpenClose} 
+      <Drawer
+        open={props.drawerOpenClose}
         variant="persistent"
         anchor="left"
         classes={{
@@ -118,53 +109,74 @@ export default function DrawerBar(props) {
       >
         <List>
           <div className="notes">
-          <ListItem button component={Link} to="/dashBoard/notes" onClick={() => { handleClass('Notes') }}
-            className={keyValue === 'Notes' ? 'pink' : 'white'} key='Notes'>
-            <ListItemIcon><EmojiObjectsOutlinedIcon /></ListItemIcon>
-            <ListItemText>Notes</ListItemText>
-          </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to="/dashBoard/notes"
+              onClick={() => {
+                handleClass("Notes");
+              }}
+              className={keyValue === "Notes" ? "pink" : "white"}
+              key="Notes"
+            >
+              <ListItemIcon>
+                <EmojiObjectsOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText>Notes</ListItemText>
+            </ListItem>
           </div>
           <div className="reminder">
-            <ListItem  >
+            <ListItem>
               <ListItemIcon>
-                <AddAlertIcon className="Reminder">
-
-                </AddAlertIcon>
+                <AddAlertIcon className="Reminder"></AddAlertIcon>
               </ListItemIcon>
-              <ListItemText primary='Reminder' />
+              <ListItemText primary="Reminder" />
             </ListItem>
           </div>
           <div className="Edit-Label">
-            <ListItem >
+            <ListItem>
               <ListItemIcon>
-                <EditIcon className="Edit Label">
-
-                </EditIcon>
+                <EditIcon className="Edit Label"></EditIcon>
               </ListItemIcon>
 
-              <ListItemText primary='Edit-Label' />
+              <ListItemText primary="Edit-Label" />
             </ListItem>
           </div>
-          <ListItem button onClick={() => { handleClass('Archive') }}
-           component={Link} to="/dashboard/archives"
-            className={keyValue === 'Archive' ? 'pink' : 'white'} key='Archive'>
-            <ListItemIcon><ArchiveOutlinedIcon /></ListItemIcon>
+          <ListItem
+            button
+            onClick={() => {
+              handleClass("Archive");
+            }}
+            component={Link}
+            to="/dashboard/archives"
+            className={keyValue === "Archive" ? "pink" : "white"}
+            key="Archive"
+          >
+            <ListItemIcon>
+              <ArchiveOutlinedIcon />
+            </ListItemIcon>
             <ListItemText>Archive</ListItemText>
           </ListItem>
-         
+
           <div className="bin">
-          <ListItem button onClick={() => { handleClass('Delete') }}
-            component={Link} to="/dashboard/trashes"
-            className={keyValue === 'Delete' ? 'pink' : 'white'} key='Delete'>
-            <ListItemIcon><Delete /></ListItemIcon>
-            <ListItemText>Trash</ListItemText>
-          </ListItem>
+            <ListItem
+              button
+              onClick={() => {
+                handleClass("Delete");
+              }}
+              component={Link}
+              to="/dashboard/trashes"
+              className={keyValue === "Delete" ? "pink" : "white"}
+              key="Delete"
+            >
+              <ListItemIcon>
+                <Delete />
+              </ListItemIcon>
+              <ListItemText>Trash</ListItemText>
+            </ListItem>
           </div>
         </List>
       </Drawer>
-     
     </div>
-
-  )
+  );
 }
-
