87001db9cdf53db20d6a4937b6ee9feae499cd54
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-02-25 16:16:42 +0530

    [Shubham]Add.Appbar Components  With Proper Formatting

--- src/components/AppBar/Appbar.jsx
+++ src/components/AppBar/Appbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./Appbar.css";
-// import Profile from "../Profile/Profile";
 import clsx from "clsx";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -9,10 +8,10 @@ import icon from "../assets/keepIcon.png";
 import SearchIcon from "@material-ui/icons/Search";
 import InputBase from "@material-ui/core/InputBase";
 import MenuIcon from "@material-ui/icons/Menu";
-import SettingsSharpIcon from '@material-ui/icons/SettingsOutlined';
+import SettingsSharpIcon from "@material-ui/icons/SettingsOutlined";
 import DnsRoundedIcon from "@material-ui/icons/DnsRounded";
 import ReplayOutlinedIcon from "@material-ui/icons/ReplayOutlined";
-import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircle';
+import AccountCircleOutlinedIcon from "@material-ui/icons/AccountCircle";
 import AppsRoundedIcon from "@material-ui/icons/AppsRounded";
 import Sidebar from "../Drawer/Drawer";
 import { makeStyles } from "@material-ui/core/styles";
@@ -20,17 +19,15 @@ import { useHistory } from "react-router-dom";
 import Menu from "@material-ui/core/Menu";
 import Paper from "@material-ui/core/Paper";
 import MenuItem from "@material-ui/core/MenuItem";
-import ListIcon from '@material-ui/icons/List';
-import AppIcon from '@material-ui/icons/Apps';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ListIcon from "@material-ui/icons/List";
+import AppIcon from "@material-ui/icons/Apps";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import { Avatar, Button } from "@material-ui/core";
 
 import Service from "../../Service/NoteService";
 var checkOpen = "close";
 
-
-
-const services = new Service()
+const services = new Service();
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -69,35 +66,32 @@ const useStyles = makeStyles((theme) => ({
   },
   settingMenu: {
     marginTop: theme.spacing(6),
-    marginLeft: "1em"
+    marginLeft: "1em",
   },
   innericon: {
     marginLeft: "7em",
-},
-row: {
-  marginTop: "10px",
-  marginLeft: "-4em",
-
-},
+  },
+  row: {
+    marginTop: "10px",
+    marginLeft: "-4em",
+  },
 }));
 
-
 export default function ToolBar(props) {
-
   const [open, setOpen, close] = React.useState(false);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [anchorE2, setAnchorE2] = React.useState(null);
   const [view, setView] = useState(false);
-  const [imageRefresh, setImageRefresh] = useState(Math.random())
+  const [imageRefresh, setImageRefresh] = useState(Math.random());
   const classes = useStyles();
   let history = useHistory();
-  const [showProfile, setShowProfile] = useState(false)
-  const [displayImage, setDisplayImage] = useState('')
-  const [hide, setHide] = useState(false)
+  const [showProfile, setShowProfile] = useState(false);
+  const [displayImage, setDisplayImage] = useState("");
+  const [hide, setHide] = useState(false);
   const [file, setFile] = React.useState("");
-  let userEmail = localStorage.getItem("email")
-  let userFirstName = localStorage.getItem("firstName")
-  let userLastName = localStorage.getItem("lastName")
+  let userEmail = localStorage.getItem("email");
+  let userFirstName = localStorage.getItem("firstName");
+  let userLastName = localStorage.getItem("lastName");
 
   const drawerOpenClose = () => {
     if (checkOpen == "open") {
@@ -109,7 +103,6 @@ export default function ToolBar(props) {
     }
     console.log(checkOpen);
   };
-
 
   const profileHandleOpen = (event) => {
     setAnchorE2(event.currentTarget);
@@ -123,50 +116,42 @@ export default function ToolBar(props) {
   };
   const handleViewOpen = () => {
     setView(!view);
-  }
+  };
   const handleHideAccount = () => {
-    setHide(!hide)
-  }
-
-
-
+    setHide(!hide);
+  };
 
   const handleProfile = async (event) => {
-
     event.preventDefault();
     setFile(event.target.files[0]);
     const formData = new FormData();
     await formData.append("file", file);
-    services.uploadImage(formData)
-        .then((response) => {
-            console.log("Image: " + JSON.stringify(response));
-            console.log("imageURL", response.data.status.imageUrl)
+    services
+      .uploadImage(formData)
+      .then((response) => {
+        console.log("Image: " + JSON.stringify(response));
+        console.log("imageURL", response.data.status.imageUrl);
 
-            localStorage.setItem("profileImage", response.data.status.imageUrl)
-
-             //const link = "http://fundoonotes.incubation.bridgelabz.com/"+localStorage.getItem('profileImage');
-
-            // localStorage.setItem('link',link)
-
-        })
-        .catch((err) => {
-            console.log("Error = " + err);
-        });
-
-};
-const link = "http://fundoonotes.incubation.bridgelabz.com/";
-
-
-
+        localStorage.setItem("profileImage", response.data.status.imageUrl);
+      })
+      .catch((err) => {
+        console.log("Error = " + err);
+      });
+  };
+  const link = "http://fundoonotes.incubation.bridgelabz.com/";
 
   return (
     <div className="main">
       {/* <Sidebar/> */}
-      <AppBar position="fixed" color="transparent" >
+      <AppBar position="fixed" color="transparent">
         <Toolbar className="topBar">
           <div className="startOptions">
             <div className="menuButton">
-              <IconButton onClick={drawerOpenClose} edge="start" aria-label="open drawer">
+              <IconButton
+                onClick={drawerOpenClose}
+                edge="start"
+                aria-label="open drawer"
+              >
                 <MenuIcon />
               </IconButton>
             </div>
@@ -192,12 +177,11 @@ const link = "http://fundoonotes.incubation.bridgelabz.com/";
               inputProps={{ "aria-label": "search" }}
             />
           </div>
-          <div className="buttonContainer">
-          </div>
+          <div className="buttonContainer"></div>
           <div class="appsContainer">
             <div className="Grid">
               <div className="button">
-                <IconButton className='App-icon' onClick={handleViewOpen} >
+                <IconButton className="App-icon" onClick={handleViewOpen}>
                   {view ? <AppIcon /> : <ListIcon />}
                   {/* <AppsRoundedIcon /> */}
                 </IconButton>
@@ -205,49 +189,50 @@ const link = "http://fundoonotes.incubation.bridgelabz.com/";
             </div>
 
             <div className="row-head3">
-  
-              <IconButton onClick={handleHideAccount} onClickAway={()=>setShowProfile(false)} alt={localStorage.getItem("firstName")}
+              <IconButton
+                onClick={handleHideAccount}
+                onClickAway={() => setShowProfile(false)}
+                alt={localStorage.getItem("firstName")}
               >
-                <AccountCircleIcon fontSize='large' />
+                <AccountCircleIcon fontSize="large" />
               </IconButton>
             </div>
-            <div className={hide ? "true profile" : "false profile"} >
+            <div className={hide ? "true profile" : "false profile"}>
               <div className="person">
                 <div className="avatarContainer">
-                <div className={classes.innericon}>
-                  <Avatar alt={localStorage.getItem("firstName")}
-                    src={link}
-                    
-                 />
-                 <div className={classes.row}>
-                <input type="file" name="file" onChange={handleProfile} />
+                  <div className={classes.innericon}>
+                    <Avatar
+                      alt={localStorage.getItem("firstName")}
+                      src={link}
+                    />
+                    <div className={classes.row}>
+                      <input type="file" name="file" onChange={handleProfile} />
+                    </div>
                   </div>
-                 </div>
                 </div>
-                <div className='name' style={{ fontSize: 20 }}>
+                <div className="name" style={{ fontSize: 20 }}>
                   {userFirstName} {userLastName}
                 </div>
-                <div className='name' style={{ fontSize: 15 }}>
+                <div className="name" style={{ fontSize: 15 }}>
                   {userEmail}
                 </div>
               </div>
               <div className="cardActions">
-                <Button variant="contained" onClick={() => {
-                  handleLogout()
-                }}>Logout</Button>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    handleLogout();
+                  }}
+                >
+                  Logout
+                </Button>
               </div>
             </div>
           </div>
-
         </Toolbar>
       </AppBar>
 
       <Sidebar drawerOpenClose={open} />
-
     </div>
   );
 }
-
-
-
-
