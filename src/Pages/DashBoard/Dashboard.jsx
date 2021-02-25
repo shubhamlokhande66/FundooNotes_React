5f0f85c590904095f319f78037c72fd4216b5da6
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-02-25 16:42:36 +0530

    [Shubham] Refactor.removed unwanted code and Proper Formatting

--- src/Pages/DashBoard/Dashboard.jsx
+++ src/Pages/DashBoard/Dashboard.jsx
@@ -1,65 +1,49 @@
-
-
-import React, { useState, useEffect } from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import { fade } from '@material-ui/core/styles';
-import InputBase from '@material-ui/core/InputBase';
-import './Dashboard.css';
-import AddNote from '../../components/CreateNotes/CreateNotes'
- import DisplayNotes from '../../components/DisplayNotes/DisplayNotes';
- import NoteService from '../../Service/NoteService';
+import React, { useState, useEffect } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import "./Dashboard.css";
+import NoteService from "../../Service/NoteService";
 import Appbar from "../../components/AppBar/Appbar";
-import Note from '../../components/Notes/Notes'
-import Archive from '../../components/Archive/Archive'
-import { Switch, Route } from 'react-router-dom'
-import Trash from '../../components/Trash/Trash'
+import Note from "../../components/Notes/Notes";
+import Archive from "../../components/Archive/Archive";
+import { Switch, Route } from "react-router-dom";
+import Trash from "../../components/Trash/Trash";
 const service = new NoteService();
-
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-
   content: {
-    marginTop: '3%',
+    marginTop: "3%",
     flexGrow: 1,
-    // paddingRight: 200, 
+    // paddingRight: 200,
   },
-
 }));
 
 export default function Dashboard() {
-
   const classes = useStyles();
   const theme = useTheme();
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const isMenuOpen = Boolean(anchorEl);
- 
-  const [search, setSearch] = useState('')
 
- 
-
-
+  const [search, setSearch] = useState("");
 
   return (
     <div className="main">
       <div className={classes.content}>
-        < Appbar/>
-        </div>
- <div className="displaynotes">
+        <Appbar />
+      </div>
+      <div className="displaynotes">
         <Switch>
-          <Route exact path="/dashBoard/notes" component={() => <Note search={search} />} />
+          <Route
+            exact
+            path="/dashBoard/notes"
+            component={() => <Note search={search} />}
+          />
           <Route exact path="/dashBoard/archives" component={Archive} />
           <Route exact path="/dashboard/trashes" component={Trash} />
-          
         </Switch>
-
-        </div>
-
-      </div> 
-     
-  )
-
+      </div>
+    </div>
+  );
 }
